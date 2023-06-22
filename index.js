7e01f517731ebffc4b0f80805c6df56e703f1ae0
Ahmad Naimi <naimi.airsh@gmail.com>
2023-06-22 18:07:24 -0400

    cyclic serverless

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
-// const connectDB = require('./config/db');
+const connectDB = require('./config/db');
 const morgan = require('morgan');
 
 // Config
@@ -12,6 +12,7 @@ dotenv.config({ path: './config/.env' });
 
 // Environment Varibles
 const PORT = process.env.PORT || 4000;
+
 
 // Start Server
 const app = express();
@@ -36,15 +37,6 @@ app.use(
 // // Routes
 app.use('/', require('./routes/index'));
 
-const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.DB_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.log(error);
-    process.exit(1);
-  }
-}
 // Server
 connectDB().then(() => {
   app.listen(PORT, () => {
