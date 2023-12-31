569409c4050cdb1f5b2ede70656a3fc57b3816b5
Ahmad Naimi <naimi.airsh@gmail.com>
2023-12-30 19:43:38 -0500

    add announcements feature

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const {getSavedPrayers, savePrayers} = require("../controllers/prayers")
+const {getSavedPrayers, savePrayers} = require("../controllers/prayers");
+const { getAnnouncements, saveAnnouncements, deleteAnnouncement } = require('../controllers/announcements');
 
 router.get('/prayers', getSavedPrayers)
+router.get('/announcements', getAnnouncements)
 
 //Body Parsing
 router.use(express.json({limit: '50mb'}));
 router.use(express.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
 
 router.post('/savePrayers', savePrayers)
+router.post('/postAnnouncements', saveAnnouncements)
+
+router.delete('/announcement/:index', deleteAnnouncement);
 
 module.exports = router;
